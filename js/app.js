0990271ad21c0b4399b406b48e152958a7e3652e
sagor20202021 <sagorahammed1562@gamil.com>
2022-03-11 01:45:39 +0600

    5no problem done

--- js/app.js
+++ js/app.js
@@ -146,6 +146,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+   document.getElementById( "liked" ).innerHTML = "";
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
@@ -154,6 +155,7 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+  // document.getElementById( "reported" ).innerHTML = "";
     const reportedPosts = getReportedPosts();
     reportedPosts.forEach((post) => {
         const div = createPost(post);
